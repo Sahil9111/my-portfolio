11cdd6d2d2ccde295d08fa92aa34dfcb4d8ff087
Sahil soni~ <sahilsoni9111@gmail.com>
2025-09-26 14:27:23 +0530

    increasing chunksize limit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    chunkSizeWarningLimit: 2000, // in KB, e.g., 2000 KB = 2 MB
+  },
   plugins: [react()],
 })
